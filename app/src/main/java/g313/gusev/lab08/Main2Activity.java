dd4c2a52e9ac8284f5baffcfbc766c2b10567cb0
Suchimauzic <sergdog03@gmail.com>
2024-02-01 20:58:48 +0300

    Finish, but I need to write additional code

--- app/src/main/java/g313/gusev/lab08/Main2Activity.java
+++ app/src/main/java/g313/gusev/lab08/Main2Activity.java
@@ -1,9 +1,12 @@
 package g313.gusev.lab08;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
@@ -24,5 +27,23 @@ public class Main2Activity extends AppCompatActivity {
         ntxt = i.getStringExtra("note-txt");
 
         txtctl.setText(ntxt);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.item_save) {
+
+        } else if (id == R.id.item_delete) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
